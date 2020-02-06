cba5b94cbd1b2f65601adcde4982dcabfcf30b6f
admin1189 <Mydreambluesky118@outlook.com>
2020-02-05 21:01:28 -0500

    create project2

---
+++ resources/views/home.blade.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no" >
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Playfair+Display:400,700,900 " rel="stylesheet">
+        <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/jquery-ui.css">
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/owl.theme.default.min.css">
+        <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+        <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+        <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+        <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+        <link rel="stylesheet" href="css/aos.css">
+
+        <link rel="stylesheet" href="css/style.css">
+
+        <!-- Styles -->
+        <style type="text/css">
+           
+            
+        </style>
+        
+        <script src="js/bootstrap.min.js"> </script>
+        
+    </head>
+    <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300" >
+
+        
+        <!-- nav-bar-->
+        <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+
+          <div class="container">
+            <div class="row align-items-center">
+              
+              <div class="col-6 col-xl-2">
+                <h1 class="mb-0 site-logo m-0 p-0"><a href="index.html" class="mb-0">#Blue sky...</a></h1>
+              </div>
+
+              <div class="col-12 col-md-10 d-none d-xl-block">
+                <nav class="site-navigation position-relative text-right" role="navigation">
+
+                  <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                    <li><a href="#home-section" class="nav-link" style="font-size: 25px;">Home</a></li>
+                    <li><a href="#properties-section" class="nav-link" style="font-size: 25px">Properties</a></li>
+                    <li><a href="#agents-section" class="nav-link" style="font-size: 25px;">Agents</a></li>
+                    <li><a href="#about-section" class="nav-link" style="font-size: 25px;">About</a></li>
+                    <li><a href="login" class="nav-link" style="font-size: 28px;">Log in </a></li>
+                    <li><a href="registration" class="nav-link" style="font-size: 25px;">Register</a></li>
+                  </ul>
+                </nav>
+              </div>
+
+
+              <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-black float-right"><span class="icon-menu h3"></span></a></div>
+
+            </div>
+          </div>
+          
+        </header>
+        <!--nav-bar-->
+        <!--main-image-->
+        <div class="site-blocks-cover " style="background-image: url(images/happy.png);"  id="home-section">
+
+
+          <div class="container">
+            <div class="row align-items-center justify-content-center">
+              <div class="col-md-6 mt-lg-5 text-center">
+                <h1>Welcome to my homepage.</h1>
+                <p class="mb-5">.</p>
+                
+              </div>
+            </div>
+          </div>
+
+          <a href="#howitworks-section" class="smoothscroll arrow-down"><span class="icon-arrow_downward"></span></a>
+        </div>
+        <!--main-image-->
+        <!--middle part-->
+        <div class="py-5 bg-light site-section how-it-works" id="howitworks-section">
+          <div class="container">
+            <div class="row mb-5 justify-content-center">
+              <div class="col-md-7 text-center">
+                <h2 class="section-title mb-3">How It Works</h2>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-4 text-center">
+                <div class="pr-5">
+                  <!-- <span class="custom-icon flaticon-house text-primary"></span>
+                  <h3 class="text-dark">Find Property.</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> -->
+                </div>
+          </div>
+
+          <div class="col-md-4 text-center">
+            <div class="pr-5">
+              <!-- <span class="custom-icon flaticon-coin text-primary"></span>
+              <h3 class="text-dark">Buy Property.</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> -->
+            </div>
+          </div>
+
+          <div class="col-md-4 text-center">
+            <div class="pr-5">
+              <!-- <span class="custom-icon flaticon-home text-primary"></span>
+              <h3 class="text-dark">Make Investment.</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> -->
+            </div>
+          </div>
+        </div>
+      </div>  
+    </div>
+
+    <section class="site-section border-bottom" id="agents-section">
+      <div class="container">
+        <div class="row mb-5">
+         
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-lg-4 mb-4">
+            <div class="team-member">
+              <figure>
+                <ul class="social">
+                  <li><a href="#"><span class="icon-facebook"></span></a></li>
+                  <li><a href="#"><span class="icon-twitter"></span></a></li>
+                  <li><a href="#"><span class="icon-linkedin"></span></a></li>
+                  <li><a href="#"><span class="icon-instagram"></span></a></li>
+                </ul>
+                <a href="property-single.html">
+              <img src="images/property_2-sm.jpg" alt="Image" class="img-fluid"></a>
+                
+              </figure>
+              <div class="p-3">
+                <h3 class="mb-2">Dashboard</h3>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-4">
+            <div class="team-member">
+              <figure>
+                <ul class="social">
+                  <li><a href="#"><span class="icon-facebook"></span></a></li>
+                  <li><a href="#"><span class="icon-twitter"></span></a></li>
+                  <li><a href="#"><span class="icon-linkedin"></span></a></li>
+                  <li><a href="#"><span class="icon-instagram"></span></a></li>
+                </ul>
+                <a href="property-single.html">
+                  <img src="images/property_3-sm.jpg" alt="Image" class="img-fluid">
+                </a>
+              </figure>
+              <div class="p-3">
+                <h3 class="mb-2">Program learning.</h3>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-4">
+            <div class="team-member">
+              <figure>
+                <ul class="social">
+                  <li><a href="#"><span class="icon-facebook"></span></a></li>
+                  <li><a href="#"><span class="icon-twitter"></span></a></li>
+                  <li><a href="#"><span class="icon-linkedin"></span></a></li>
+                  <li><a href="#"><span class="icon-instagram"></span></a></li>
+                </ul>
+                <a href="property-single.html">
+              <img src="images/property_1-sm.jpg" alt="Image" class="img-fluid">
+            </a>
+              </figure>
+              <div class="p-3">
+                <h3 class="mb-2">Other..</h3>
+                
+              </div>
+            </div>
+          </div>        
+        </div>
+      </div>
+    </section>
+    <!--middle part-->
+
+    <!--footer part-->
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <div class="row">
+              <div class="col-md-5">
+                <h2 class="footer-heading mb-4">About this site</h2>
+                <p>This site provide various function neccssary.Through visit with this site,Peoples could get conveinent service.</p>
+              </div>
+              <div class="col-md-3 ml-auto">
+                <h2 class="footer-heading mb-4">Quick Links</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="#">Testimonials</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="mb-4">
+              <h2 class="footer-heading mb-4">Subscribe Newsletter</h2>
+            <form action="#" method="post" class="footer-subscribe">
+              <div class="input-group mb-3">
+                <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
+                <div class="input-group-append">
+                  <button class="btn btn-primary text-black" type="button" id="button-addon2">Send</button>
+                </div>
+              </div>
+            </form>  
+            </div>
+            
+            <div class="">
+              <h2 class="footer-heading mb-4">Follow Us</h2>
+                <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+            </div>
+
+
+          </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <div class="border-top pt-5">
+            <p>
+        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        Copyright &copy; Current Day - >   <script>
+       
+        // document.write(new Date().getFullYear());
+        var y = new Date().getFullYear();
+        var m = new Date().getMonth()+1;
+        var d = new Date().getDate();
+        res=y+": "+m+":"+d;
+        document.write(res);
+        </script> < - | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="http://mydev1214.wixsite.com/gannady-site" target="_blank" >Bluesky man</a>
+        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        </p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+
+    <!--script-->
+      <script src="js/jquery-3.3.1.min.js"></script>
+      <script src="js/jquery-migrate-3.0.1.min.js"></script>
+      <script src="js/jquery-ui.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/jquery.stellar.min.js"></script>
+      <script src="js/jquery.countdown.min.js"></script>
+      <script src="js/bootstrap-datepicker.min.js"></script>
+      <script src="js/jquery.easing.1.3.js"></script>
+      <script src="js/aos.js"></script>
+      <script src="js/jquery.fancybox.min.js"></script>
+      <script src="js/jquery.sticky.js"></script>
+
+      
+      <script src="js/main.js"></script>
+    </body>
+</html>
